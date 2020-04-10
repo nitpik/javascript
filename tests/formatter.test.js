c90213a5029d5b50062e67346900569911590f3a
Nicholas C. Zakas <nicholas@nczconsulting.com>
2020-04-10 15:10:14 -0700

    Fix: Ensure space after 'typeof' operator (fixes #5) (#6)

--- tests/formatter.test.js
+++ tests/formatter.test.js
@@ -237,7 +237,7 @@ a(\`hello \${
             const [ options, source, expected ] = contents.split("\n---\n");
             
 
-            // if (!fileName.includes("functions")) return;
+            // if (!fileName.includes("operator")) return;
             it(`Test in ${ fileName } should format correctly`, () => {
                 const formatter = new Formatter({
                     style: JSON.parse(options)
